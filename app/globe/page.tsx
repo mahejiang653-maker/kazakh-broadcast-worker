30a8a11dc968522b5b29570382b7dd64c7859ce6
mahejiang653-maker <mahejiang653@gmail.com>
2026-09-06 05:57:18 +0800

    Switch globe page to V38

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -19,7 +19,7 @@ export default function GlobePage() {
       }}
     >
       <iframe
-        src="/news-globe-v37.html?v=37"
+        src="/news-globe-v38.html?v=38"
         title="全球新闻十三地新闻地球仪"
         style={{
           display: "block",
