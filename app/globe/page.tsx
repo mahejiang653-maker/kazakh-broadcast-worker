f19f3baee2d695863a7423231c2ed3efd1a35a96
mahejiang653-maker <mahejiang653@gmail.com>
2026-09-06 02:45:52 +0800

    Point globe route to V27 strategy effects

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -19,7 +19,7 @@ export default function GlobePage() {
       }}
     >
       <iframe
-        src="/news-globe-v14.html?v=26"
+        src="/news-globe-v14.html?v=27"
         title="全球新闻十三地新闻地球仪"
         style={{
           display: "block",
