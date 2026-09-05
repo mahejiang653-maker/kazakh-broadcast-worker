13d59f5d439c3bc07add03c0d7b9ba6acb987a53
mahejiang653-maker <mahejiang653@gmail.com>
2026-09-05 12:39:30 +0800

    Point globe route to optimized V14 build

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -19,7 +19,7 @@ export default function GlobePage() {
       }}
     >
       <iframe
-        src="/news-globe-v14.html?v=18"
+        src="/news-globe-v14.html?v=19"
         title="全球新闻十三地新闻地球仪"
         style={{
           display: "block",
